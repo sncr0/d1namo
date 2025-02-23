215214b8cd3d8b04767b64c91a4991af99c3358d
JNikolo <125705821+JNikolo@users.noreply.github.com>
2025-02-23 03:40:22 -0500

    fix: Fix live data page

--- client/gludaily/app/(Pages)/LiveDataPage.tsx
+++ client/gludaily/app/(Pages)/LiveDataPage.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
-import LiveGraph from '@/components/LiveGraph';
-import { useRouter } from 'expo-router';
+import React from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import LiveGraph from "@/components/LiveGraph";
+import { useRouter } from "expo-router";
 
 const router = useRouter();
 
 const LiveDataPage = () => (
   <View style={styles.container}>
     <LiveGraph />
-    <TouchableOpacity onPress={() => router.replace('/chatBot')} >
+    <TouchableOpacity onPress={() => router.replace("/chatBot")}>
       <Text style={styles.backButton}>Back</Text> {/* Updated here */}
     </TouchableOpacity>
   </View>
@@ -17,13 +17,13 @@ const LiveDataPage = () => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black' // Set the background color to white
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black", // Set the background color to white
   },
   backButton: {
-    color: 'white' // Set the text color to white
-  }
+    color: "white", // Set the text color to white
+  },
 });
 
 export default LiveDataPage;
