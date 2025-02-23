7160d05338d299739eff9d1a47b92cd27cd55698
Sam <samuel.moerman@gmail.com>
2025-02-22 20:49:20 -0500

    backend calls

--- backend/models/glucose.py
+++ backend/models/glucose.py
@@ -2,9 +2,9 @@ import pandas as pd
 import os
 
 
-def get_glucose_data():
+def load_glucose_sample():
     print(os.getcwd())
     glucose_sample_path = '../data/glucose.csv'  # maybe need to update
     glucose_sample_df = pd.read_csv(glucose_sample_path, memory_map=True)
-    data = glucose_sample_df[glucose_sample_df['type'] == "cgm"][0:100][['date', 'time', 'glucose']]
-    return data['date'], data['time'], data['glucose']
+    data = glucose_sample_df[glucose_sample_df['type'] == "cgm"]
+    return data
